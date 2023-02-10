325ad0547bc4c8550e325451ab59ecbf63f263a4
renato1982 <42810458+renato1982@users.noreply.github.com>
2023-02-09 22:33:38 -0300

    Delete mmcafe directory

--- mmcafe/src/main/java/com/mmcafe/upload/service/UploadFileService.java
+++
@@ -1,8 +0,0 @@
-package com.mmcafe.upload.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UploadFileService {
-
-    String inserirImagem(MultipartFile file);
-}
